c02ff3c3eb25bc878d243e1d328a7b4539dfff9c
Sasa Nikolic <sasanikolic@Sasa-Nikolics-iMac.local>
2015-10-06 21:24:48 +1100

    Initial Commit

---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Sasa Nikolic on 6/10/15.
+//  Copyright (c) 2015 Ark & Nova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Sasa Nikolic on 6/10/15.
+//  Copyright (c) 2015 Ark & Nova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
